af10bc84bcd696469ee03d3333552d763282adf4
abhi972527 <akc972527@gmail.com>
2021-11-14 13:02:00 +0530

    added login signup functionality

---
+++ nav/navbar.js
@@ -0,0 +1,13 @@
+function navbar () {
+
+    return `
+    
+    <h3> <a href="/">Home</a> </h3>
+    <div id="options">
+        <h3><a href="login.html">Sign In</a></h3>
+        <h3><a href="signup.html">Sign Up</a></h3>
+    </div>`
+
+}
+
+export default navbar;
